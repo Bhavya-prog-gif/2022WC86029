f780de23de5e25b510c1b9009c336ad560eb452c
BHAVYA RAI <2022wc86029@wilp.bits-pilani.ac.in>
2026-09-25 11:56:59 +0530

    Update source code for continuous integration

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -1,8 +1,8 @@
 package com.bits;
 
 public class App {
-
     public static void main(String[] args) {
         System.out.println("Hello DevOps!");
+        System.out.println("Continuous Integration is working!");
     }
 }
